29a74f935269f005a934c53407fc16cca53d8371
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-19 19:10:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    binding.pry
+    raise params
     Recipe.create(name: params[:name],
                   ingredients: params[:ingredients],
                   cook_time: params[:cook_time])
